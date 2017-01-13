c4889f9fde8ee84223f13a86b86d34052ddb847f
trc-forever <921255465@qq.com>
2017-01-13 20:12:16 +0800

    。

--- index.js
+++ index.js
@@ -13,7 +13,26 @@ const io = socket(server);
 app
   .use(async(ctx, next) => {
     let file = ctx.path;
-    await next();
+    if (file === '/') file = 'index.html';
+    // 拦截请求html文件
+    if (/\.html/.test(file) === true) {
+      let absolute = path.join(__dirname, file);
+      let html = fs.readFileSync(absolute).toString();
+      html = html.split('</head>');
+      html = html.join(`
+        <script src="/socket.io/socket.io.js"></script>
+        <script>
+          window.onload = function() {
+            const socket = io();
+            socket.on('reload', () => {
+            window.location.reload();
+            })
+          }
+        </script></head>`);
+      ctx.body = html;
+    } else { // 不是html文件则转到静态文件服务器
+      await next();
+    }
     createWatch(file);
   })
   // 将服务器设置为基本的静态文件服务器
@@ -26,7 +45,6 @@ server.listen('9000', () => {
 let wathcers = {};
 
 function createWatch(file) {
-  if (file === '/') file = 'index.html';
   let absolute = path.join(__dirname, file);
   if (wathcers[absolute] === true) return;
   fs.watch(absolute, (e, filename) => {
